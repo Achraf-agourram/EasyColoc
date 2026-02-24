ce5cb4045fa630ef7ea3d0b1de02e5d8288fd94e
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-24 14:44:41 +0000

    filled colocations table

--- laravel/database/migrations/2026_02_24_142637_create_colocations_table.php
+++ laravel/database/migrations/2026_02_24_142637_create_colocations_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
+            $table->string('token')->unique();
+            $table->boolean('is_active')->default(true);
+            
+            $table->foreignId('owner_id')->constrained('persons')->cascadeOnDelete();
             $table->timestamps();
         });
     }
