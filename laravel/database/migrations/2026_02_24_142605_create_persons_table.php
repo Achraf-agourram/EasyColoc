6d5c7b7fba2d0392f3d5fead59f5a72a6bddf0d3
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-24 22:49:55 +0000

    filled categories table

--- laravel/database/migrations/2026_02_24_142605_create_persons_table.php
+++ laravel/database/migrations/2026_02_24_142605_create_persons_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('userId')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
 
             $table->boolean('isOwner')->default(false);
             $table->boolean('banned')->default(false);
