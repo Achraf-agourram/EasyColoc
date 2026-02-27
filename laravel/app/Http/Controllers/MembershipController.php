1aff59aa7f6a4906cf83ba13c2cb395290ee029f
Achraf-podcast <achrafagourram6@gmail.com>
2026-02-27 12:27:31 +0000

    fix: added the limit to join just one colocation

--- laravel/app/Http/Controllers/MembershipController.php
+++ laravel/app/Http/Controllers/MembershipController.php
@@ -13,13 +13,17 @@ class MembershipController extends Controller
     public function join (Request $request)
     {
         $colocationToJoin = Colocation::where('token', $request->token)->firstOrFail();
-        
-        Membership::create([
-            'role' => 'member',
-            'joinedAt' => Carbon::today(),
-            'user_id' => Auth::id(),
-            'colocation_id' => $colocationToJoin->id,
-        ]);
+
+        $membership = auth()->user()->memberships()->where('isActive', true)->first();
+
+        if (!$membership) {
+            Membership::create([
+                'role' => 'member',
+                'joinedAt' => Carbon::today(),
+                'user_id' => Auth::id(),
+                'colocation_id' => $colocationToJoin->id,
+            ]);
+        }
 
         return redirect('/colocation/'.$request->token);
     }
